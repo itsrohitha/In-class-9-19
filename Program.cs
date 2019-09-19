b3a815162d313f8526c84a892bbe4a78e9e921a4
Naga Rohitha Nannapaneni <nagarohitha@usf.edu>
2019-09-19 16:25:49 -0400

    Changes

--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ namespace In_class_9_19
         {
             Console.WriteLine("Please enter your name: ");
             string name = Console.ReadLine();
+
             Console.WriteLine("Please enter your age: ");
             int age = int.Parse(Console.ReadLine());
+
             Console.WriteLine("Please enter your grades for your 4 classes: ");
             int[] grade = new int[5];
             grade[1] = int.Parse(Console.ReadLine());
@@ -17,11 +19,11 @@ namespace In_class_9_19
             grade[3] = int.Parse(Console.ReadLine());
             grade[4] = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(name);
-            Console.WriteLine(age);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
          
             if (grade[1] > 90)
-                Console.WriteLine("you got an A");
+                Console.WriteLine("you got an A!!!");
             else if (grade[1] < 89 && grade[1] > 80)
                 Console.WriteLine("you get a B");
             else if (grade[1] < 79 && grade[1] > 70)
@@ -32,7 +34,7 @@ namespace In_class_9_19
                 Console.WriteLine("you get a F");
 
             if (grade[2] > 90)
-                Console.WriteLine("you got an A");
+                Console.WriteLine("you got an A!!!");
             else if (grade[2] < 89 && grade[2] > 80)
                 Console.WriteLine("you get a B");
             else if (grade[2] < 79 && grade[2] > 70)
@@ -43,7 +45,7 @@ namespace In_class_9_19
                 Console.WriteLine("you get a F");
 
             if (grade[3] > 90)
-                Console.WriteLine("you got an A");
+                Console.WriteLine("you got an A!!!");
             else if (grade[3] < 89 && grade[3] > 80)
                 Console.WriteLine("you get a B");
             else if (grade[3] < 79 && grade[3] > 70)
@@ -54,7 +56,7 @@ namespace In_class_9_19
                 Console.WriteLine("you get a F");
 
             if (grade[4] > 90)
-                Console.WriteLine("you got an A");
+                Console.WriteLine("you got an A!!!");
             else if (grade[4] < 89 && grade[4] > 80)
                 Console.WriteLine("you get a B");
             else if (grade[4] < 79 && grade[4] > 70)
